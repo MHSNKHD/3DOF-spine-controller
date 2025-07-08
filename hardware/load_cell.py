dfe5b8fc1e98633963873d246d1e7a55ac07daad
Mohsen Khodaee <mkhodaee92@gmail.com>
2025-07-08 09:11:31 +0200

    load_cell and load_cell_bota

--- hardware/load_cell.py
+++ hardware/load_cell.py
@@ -1,7 +1,108 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Jul  7 13:34:20 2025
+# load_cell.py
+# oliverwigger
 
-@author: mkhod
-"""
+import hardware.load_cell_bota as load_cell_bota
+import config as cfg
+import logging as log
+import time
+# from gui import processFrame
+#import tkinter as tk
 
+lc = None
+
+def start():
+    global lc
+    
+    try:
+        lc = load_cell_bota.BotaSerialSensor(cfg.LC["PORT"])
+        lc.start()
+        log.info("LoadCell started")
+        #processFrame.bt_homing.config(state=tk.NORMAL, bg=cfg.COL["en"])
+    except:
+        print("error")
+        log.error("LoadCell FAILED")
+        
+    lc._fx0.value = cfg.LC0F[0]
+    lc._fy0.value = cfg.LC0F[1]
+    lc._fz0.value = cfg.LC0F[2]
+    
+    lc._tx0.value = cfg.LC0T[0]
+    lc._ty0.value = cfg.LC0T[1]
+    lc._tz0.value = cfg.LC0T[2]
+    
+def stop():
+    lc.stop()
+
+def zero():
+    time.sleep(3)
+    log.debug("Zero LoadCell...")
+    fx_sum = []
+    fy_sum = []
+    fz_sum = []
+    tx_sum = []
+    ty_sum = []
+    tz_sum = []
+
+    global lc
+    for _ in range(cfg.LC["N_ZERO"]):
+        fx_sum.append(lc._f[0])
+        fy_sum.append(lc._f[1])
+        fz_sum.append(lc._f[2])
+
+        tx_sum.append(lc._t[0])
+        ty_sum.append(lc._t[1])
+        tz_sum.append(lc._t[2])
+
+        time.sleep(0.1)
+        
+    fx_sum.sort()
+    fy_sum.sort()
+    fz_sum.sort()
+    
+    tx_sum.sort()
+    ty_sum.sort()
+    tz_sum.sort()
+    
+    start = int(cfg.LC["N_ZERO"]*0.15)
+    end = int(cfg.LC["N_ZERO"]*0.85)
+    
+    fx_sum = fx_sum[start:end]
+    fy_sum = fy_sum[start:end]
+    fz_sum = fz_sum[start:end]
+    
+    tx_sum = tx_sum[start:end]
+    ty_sum = ty_sum[start:end]
+    tz_sum = tz_sum[start:end]
+    
+    cfg.LC0F = [round(sum(fx_sum) / len(fx_sum), 5), round(sum(fy_sum) / len(fy_sum), 5), round(sum(fz_sum) / len(fz_sum), 5)]
+    cfg.LC0T = [round(sum(tx_sum) / len(tx_sum), 5), round(sum(ty_sum) / len(ty_sum), 5), round(sum(tz_sum) / len(tz_sum), 5)]
+    
+    lc._fx0.value = cfg.LC0F[0]
+    lc._fy0.value = cfg.LC0F[1]
+    lc._fz0.value = cfg.LC0F[2]
+    
+    lc._tx0.value = cfg.LC0T[0]
+    lc._ty0.value = cfg.LC0T[1]
+    lc._tz0.value = cfg.LC0T[2]
+    
+    log.info(
+        "\nLOAD_CELL_ZERO \n"
+        + "LC0F: "
+        + str(cfg.LC0F)
+        + " N,\n"
+        + "LC0T: "
+        + str(cfg.LC0T)
+        + " Nm,\n"
+    )
+
+    log.debug("Zero LoadCell DONE")
+
+
+def log_all():
+    log.debug("Fx: " + str(lc._f[0]))
+    log.debug("Fy: " + str(lc._f[1]))
+    log.debug("Fz: " + str(lc._f[2]))
+
+    log.debug("Mx: " + str(lc._t[0]))
+    log.debug("My: " + str(lc._t[1]))
+    log.debug("Mz: " + str(lc._t[2]))
---
+++ hardware/load_cell_bota.py
@@ -0,0 +1,279 @@
+# load_cell_bota.py
+# oliverwigger
+
+import struct
+import time
+import threading
+
+from collections import namedtuple
+
+import serial
+import logging as log
+import gui.logFrame as logFrame
+from multiprocessing import Process
+import multiprocessing
+import ctypes
+from crc import Calculator, Configuration
+
+
+class BotaSerialSensor:
+    BOTA_PRODUCT_CODE = 123456
+    BAUDERATE = 460800
+    SINC_LENGTH = 512  # 512
+    CHOP_ENABLE = 0
+    FAST_ENABLE = 0
+    FIR_DISABLE = 1
+    TEMP_COMPENSATION = 0  # 0: Disabled (recommended), 1: Enabled
+    USE_CALIBRATION = 1  # 1: calibration matrix active, 0: raw measurements
+    DATA_FORMAT = 0  # 0: binary, 1: CSV
+    BAUDERATE_CONFIG = 4  # 0: 9600, 1: 57600, 2: 115200, 3: 230400, 4: 460800
+    FRAME_HEADER = b"\xAA"
+    # Note that the time step is set according to the sinc filter size!
+    time_step = 0.01
+    proc_thread = None
+
+    def __init__(self, port):
+        self._port = port
+        self._ser = serial.Serial(timeout=0.1)
+        self._pd_thread_stop_event = threading.Event()
+        DeviceSet = namedtuple("DeviceSet", "name product_code config_func")
+        self._expected_device_layout = {
+            0: DeviceSet(
+                "BFT-LAXS-SER-M8", self.BOTA_PRODUCT_CODE, self.bota_sensor_setup
+            )
+        }
+        self._status = None
+
+        # Multiprocess Arrays
+        self._f = multiprocessing.Array(ctypes.c_double, (0.0, 0.0, 0.0))
+        self._t = multiprocessing.Array(ctypes.c_double, (0.0, 0.0, 0.0))
+
+        self._fx0 = multiprocessing.Value(ctypes.c_float, 0.0)
+        self._fy0 = multiprocessing.Value(ctypes.c_float, 0.0)
+        self._fz0 = multiprocessing.Value(ctypes.c_float, 0.0)
+        
+        self._tx0 = multiprocessing.Value(ctypes.c_float, 0.0)
+        self._ty0 = multiprocessing.Value(ctypes.c_float, 0.0)
+        self._tz0 = multiprocessing.Value(ctypes.c_float, 0.0)
+        
+        
+        # added variables
+        self._n_measured = 0
+        self._lc_is_on = 0
+    
+    
+    #sub process
+    #####################################################
+    def _processdata_thread_nocrc(self):
+        i = 0
+        t_start = time.time()
+        tm = 0
+        IS_TM_RESET = 0 
+        
+        while not self._pd_thread_stop_event.is_set():
+            frame_synced = False
+            crc16X25Configuration = Configuration(
+                16, 0x1021, 0xFFFF, 0xFFFF, True, True
+            )
+            crc_calculator = Calculator(crc16X25Configuration)
+
+            while not frame_synced and not self._pd_thread_stop_event.is_set():
+                possible_header = self._ser.read(1)
+                if self.FRAME_HEADER == possible_header:
+                    data_frame = self._ser.read(34)
+                    crc16_ccitt_frame = self._ser.read(2)
+                    
+                    crc16_ccitt = struct.unpack_from("H", crc16_ccitt_frame, 0)[0]
+                    checksum = crc_calculator.checksum(data_frame)
+                    if checksum == crc16_ccitt:
+                        #print("Frame synced")
+                        frame_synced = True
+                    else:
+                        self._ser.read(1)
+                        
+                    while frame_synced and not self._pd_thread_stop_event.is_set():
+                        frame_header = self._ser.read(1)
+
+                        if frame_header != self.FRAME_HEADER:
+                            #print("Lost sync")
+                            frame_synced = False
+                            break
+
+                        data_frame = self._ser.read(34)
+                        crc16_ccitt_frame = self._ser.read(2)
+
+                        crc16_ccitt = struct.unpack_from("H", crc16_ccitt_frame, 0)[0]
+                        checksum = crc_calculator.checksum(data_frame)
+                        if checksum != crc16_ccitt:
+                            #print("CRC mismatch received")
+                            break
+                        
+                        self._status = struct.unpack_from("H", data_frame, 0)[0]
+                        
+                        fx_temp = round(struct.unpack_from("f", data_frame, 2)[0] - self._fx0.value,6)
+                        if abs(fx_temp) < 500: #filter heavy outliers
+                            if abs(fx_temp) - abs(self._f[0]) < 300:
+                                self._f[0] = fx_temp
+                        
+                        fy_temp = round(struct.unpack_from("f", data_frame, 6)[0] - self._fy0.value,6)
+                        if abs(fy_temp) < 500: #filter heavy outliers
+                            if abs(fy_temp) - abs(self._f[1]) < 300:
+                                self._f[1] = fy_temp
+                        
+                        fz_temp = round(struct.unpack_from("f", data_frame, 10)[0] - self._fz0.value,6)
+                        if abs(fz_temp) < 500: #filter heavy outliers
+                            if abs(fz_temp) - abs(self._f[2]) < 300:
+                                self._f[2] = 0 #fz_temp
+                        
+                        #torque
+                        tx_temp = -round(struct.unpack_from("f", data_frame, 18)[0] - self._tx0.value,6)
+                        if abs(tx_temp) < 20: #filter heavy outliers
+                            if abs(tx_temp) - abs(self._t[0]) < 10:
+                                self._t[0] = tx_temp
+                        
+                        ty_temp = round(struct.unpack_from("f", data_frame, 14)[0] - self._ty0.value,6)
+                        if abs(ty_temp) < 20: #filter heavy outliers
+                            if abs(ty_temp) - abs(self._t[1]) < 10:
+                                self._t[1] = ty_temp
+                            
+                        tz_temp = round(struct.unpack_from("f", data_frame, 22)[0] - self._tz0.value,6)
+                        if abs(tz_temp) < 20: #filter heavy outliers
+                            if abs(tz_temp) - abs(self._t[2]) < 10:
+                                self._t[2] = tz_temp
+
+                        i = i+1
+                        '''
+                        print('\n ------------------')
+                        print(f"Fx: {self._f[0]}")
+                        print(f"Fy: {self._f[1]}")
+                        print(f"Fz: {self._f[2]}")
+                        
+                        print(f"Tx: {self._t[0]}")
+                        print(f"Ty: {self._t[1]}")
+                        print(f"Tz: {self._t[2]}")
+                        time.sleep(0.001)
+                        '''
+                        
+                        if i >= 1000:
+                            #log.debug(f"LoadCell: {round(i/(time.time()-t_start),2)} Hz")
+                            i = 0
+                            t_start = time.time()
+                        
+                        
+    def bota_sensor_setup(self):
+        log.debug("BOTA: Trying to setup the sensor.")
+        # Wait for streaming of data
+        out = self._ser.read_until(bytes("App Init", "ascii"))
+        if not self.contains_bytes(bytes("App Init", "ascii"), out):
+            log.debug("BOTA: Sensor not streaming, check if correct port selected!")
+            return False
+        time.sleep(0.5)
+        self._ser.reset_input_buffer()
+        self._ser.reset_output_buffer()
+
+        # Go to CONFIG mode
+        cmd = bytes("C", "ascii")
+        self._ser.write(cmd)
+        out = self._ser.read_until(bytes("r,0,C,0", "ascii"))
+        if not self.contains_bytes(bytes("r,0,C,0", "ascii"), out):
+            log.debug("BOTA: Failed to go to CONFIG mode.")
+            return False
+
+        # Communication setup
+        comm_setup = f"c,{self.TEMP_COMPENSATION},{self.USE_CALIBRATION},{self.DATA_FORMAT},{self.BAUDERATE_CONFIG}"
+        # print(comm_setup)
+        cmd = bytes(comm_setup, "ascii")
+        self._ser.write(cmd)
+        out = self._ser.read_until(bytes("r,0,c,0", "ascii"))
+        if not self.contains_bytes(bytes("r,0,c,0", "ascii"), out):
+            log.debug("BOTA: Failed to set communication setup.")
+            return False
+        time_step = 0.00001953125 * self.SINC_LENGTH
+        log.debug("BOTA: Timestep: {}".format(time_step))
+
+        # Filter setup
+        filter_setup = f"f,{self.SINC_LENGTH},{self.CHOP_ENABLE},{self.FAST_ENABLE},{self.FIR_DISABLE}"
+        # print(filter_setup)
+        cmd = bytes(filter_setup, "ascii")
+        self._ser.write(cmd)
+        out = self._ser.read_until(bytes("r,0,f,0", "ascii"))
+        if not self.contains_bytes(bytes("r,0,f,0", "ascii"), out):
+            log.debug("BOTA: Failed to set filter setup.")
+            return False
+
+        # Go to RUN mode
+        cmd = bytes("R", "ascii")
+        self._ser.write(cmd)
+        out = self._ser.read_until(bytes("r,0,R,0", "ascii"))
+        if not self.contains_bytes(bytes("r,0,R,0", "ascii"), out):
+            log.debug("BOTA: Failed to go to RUN mode.")
+            return False
+
+        return True
+
+    def contains_bytes(self, subsequence, sequence):
+        return subsequence in sequence
+
+    def start(self):
+        global _lc_is_on
+
+        self._ser.baudrate = self.BAUDERATE
+        self._ser.port = self._port
+        self._ser.timeout = 10
+
+        try:
+            self._ser.open()
+            log.debug("BOTA: Opened serial port {}".format(self._port))
+        except serial.SerialException as var:
+            logFrame.LogWindow.write_log("Load Cell not connected")
+            print("An Exception Occured")
+            print("Exception Details-> ", var)
+            # log.debug('BOTA: Could not open port')
+            # raise BotaSerialSensorError('Could not open port')
+
+        if not self._ser.is_open:
+            log.debug("BOTA: Could not open port")
+            raise BotaSerialSensorError("Could not open port")
+
+        if not self.bota_sensor_setup():
+            log.debug("Could not setup sensor!")
+            return
+        
+        self._lc_is_on = 1
+        
+        # multiprocess
+        proc = Process(target=self._processdata_thread_nocrc, name="load_cell_multiprocess")
+        proc.start()
+        time.sleep(1)
+        
+        #proc_thread = threading.Thread(
+        #    target=self._processdata_thread_nocrc, name="LoadCell"
+        #)
+        #proc_thread.start()
+        
+        
+
+    def stop(self):
+        self._lc_is_on = 0
+        self._pd_thread_stop_event.set()
+        self.proc_thread.join()
+
+        self._ser.close()
+
+    @staticmethod
+    def _sleep(duration, get_now=time.perf_counter):
+        now = get_now()
+        end = now + duration
+        while now < end:
+            now = get_now()
+
+
+class BotaSerialSensorError(Exception):
+    def __init__(self, message):
+        super(BotaSerialSensorError, self).__init__(message)
+        self.message = message
+
+
+#my helper function ;-)
+def _is_in_range(new, old, tolerance):
+    return int(abs(new - old) <= tolerance)
