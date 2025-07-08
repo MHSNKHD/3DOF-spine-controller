9bfe45732bb036cb8c187c2e52c0483c6d116917
Mohsen Khodaee <mkhodaee92@gmail.com>
2025-07-08 16:06:46 +0200

    Update test_dummy.py

--- tests/test_dummy.py
+++ tests/test_dummy.py
@@ -1,6 +1,14 @@
-import serial
+from hardware import load_cell
+import time
 
-ser = serial.Serial('/dev/ttyUSB0', 460800, timeout=1)
-print("Serial port opened successfully.")
-ser.close()
+load_cell.start()
 
+for _ in range(10):
+    fx, fy, fz = load_cell.get_forces()
+    mx, my, mz = load_cell.get_moments()
+    print(f"Fx: {fx:.2f} N, Fy: {fy:.2f} N, Fz: {fz:.2f} N")
+    print(f"Mx: {mx:.2f} Nm, My: {my:.2f} Nm, Mz: {mz:.2f} Nm")
+    print("---------------------")
+    time.sleep(0.5)
+
+load_cell.stop()
