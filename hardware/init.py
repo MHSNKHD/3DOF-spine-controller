1347568d17bd98964ed9c467d45b8166453fc08f
Mohsen Khodaee <mkhodaee92@gmail.com>
2025-07-08 17:37:47 +0200

    Create init.py

---
+++ hardware/init.py
@@ -0,0 +1,18 @@
+# init.py
+# oliverwigger
+
+from hardware import encoder
+from hardware import fogger
+from hardware import led
+from hardware import motor
+from hardware import instron
+
+
+def init():
+    #led.init()
+    motor.init()
+    #instron.init()
+    encoder.init()
+    fogger.init()
+    #led.switch_to("GREEN")
+    motor.stop_all()
