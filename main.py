f22de61a8822ee959b850f0d9705f39ba071ae6f
Mohsen Khodaee <mkhodaee92@gmail.com>
2025-07-08 18:01:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,7 @@ from hardware import motor, load_cell
 load_cell.start()
 motor.init()
 
-# Apply 200 N in X
-motor.apply_force("X", 20)
-
-# Apply 2 Nm around Z
-motor.apply_moment("Z", 1.0)
+# Apply 20 N in Z
+motor.apply_force("Z", 20)
 
 
