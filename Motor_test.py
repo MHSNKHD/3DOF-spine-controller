9e74813dc2b69e27fde80ec5be833155212c3081
Mohsen Khodaee <mkhodaee92@gmail.com>
2025-07-09 16:13:09 +0200

    Update Motor_test.py

--- Motor_test.py
+++ Motor_test.py
@@ -7,43 +7,43 @@ motor.init()
 # Test X axis
 print("Testing X+")
 motor.mot_pos("X")
-time.sleep(1)
+time.sleep(0.5)
 motor.mot_stop("X")
 
-time.sleep(1)
+time.sleep(0.5)
 
 print("Testing X-")
 motor.mot_neg("X")
-time.sleep(1)
+time.sleep(0.5)
 motor.mot_stop("X")
 
 
 # Test Y axis
 print("Testing Y+")
 motor.mot_pos("Y")
-time.sleep(1)
+time.sleep(0.5)
 motor.mot_stop("Y")
 
-time.sleep(1)
+time.sleep(0.5)
 
 print("Testing Y-")
 motor.mot_neg("Y")
-time.sleep(1)
+time.sleep(0.5)
 motor.mot_stop("Y")
 
-time.sleep(1)
+time.sleep(0.5)
 
 # Test Z axis
 print("Testing Z+")
 motor.mot_pos("Z")
-time.sleep(1)
+time.sleep(0.5)
 motor.mot_stop("Z")
 
-time.sleep(1)
+time.sleep(0.5)
 
 print("Testing Z-")
 motor.mot_neg("Z")
-time.sleep(1)
+time.sleep(0.5)
 motor.mot_stop("Z")
 
 motor.stop_all()
