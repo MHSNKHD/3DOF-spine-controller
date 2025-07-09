a4ab81fc459be1af75bf5f3ebda6634225ddd51b
Mohsen Khodaee <mkhodaee92@gmail.com>
2025-07-09 16:10:51 +0200

    motor_test

---
+++ Motor_test.py
@@ -0,0 +1,49 @@
+# test_motor_direction.py
+import time
+import config as cfg
+from hardware import motor
+
+motor.init()
+# Test X axis
+print("Testing X+")
+motor.mot_pos("X")
+time.sleep(1)
+motor.mot_stop("X")
+
+time.sleep(1)
+
+print("Testing X-")
+motor.mot_neg("X")
+time.sleep(1)
+motor.mot_stop("X")
+
+
+# Test Y axis
+print("Testing Y+")
+motor.mot_pos("Y")
+time.sleep(1)
+motor.mot_stop("Y")
+
+time.sleep(1)
+
+print("Testing Y-")
+motor.mot_neg("Y")
+time.sleep(1)
+motor.mot_stop("Y")
+
+time.sleep(1)
+
+# Test Z axis
+print("Testing Z+")
+motor.mot_pos("Z")
+time.sleep(1)
+motor.mot_stop("Z")
+
+time.sleep(1)
+
+print("Testing Z-")
+motor.mot_neg("Z")
+time.sleep(1)
+motor.mot_stop("Z")
+
+motor.stop_all()
