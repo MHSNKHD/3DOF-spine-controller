78fa3edd0922133f8c9a9d5ce65b1c3a7044c9df
Mohsen Khodaee <mkhodaee92@gmail.com>
2025-07-10 11:00:43 +0200

    Update test_motor_nudge.py

--- test_motor_nudge.py
+++ test_motor_nudge.py
@@ -5,20 +5,27 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-# Set up both motor pins
-GPIO.setup(24, GPIO.OUT)  # XP (must be LOW)
-GPIO.setup(23, GPIO.OUT)  # XN (we'll set HIGH)
+
+# Test X motor
+GPIO.setup(24, GPIO.OUT)  # XP 
+GPIO.setup(23, GPIO.OUT)  # XN 
+
 
 # Wait before starting
 time.sleep(1)
 
-print("Testing negative direction (X-)")
+# Positive
+GPIO.output(24,1)
+GPIO.output(23,0)
+time.sleep(1)
+GPIO.output(24,0)
+time.sleep(1) 
 
-# XP LOW, XN HIGH = Negative rotation
+#Negative
 GPIO.output(24, 0)
 GPIO.output(23, 1)
-
-time.sleep(1)  # Rotate for 1 second
+time.sleep(1)
+GPIO.output(23, 0)
 
 # Stop motor
 GPIO.output(23, 0)
