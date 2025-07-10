0934a8daaa569e930670d6a32efa332e55e8e0a5
Mohsen Khodaee <mkhodaee92@gmail.com>
2025-07-10 17:44:27 +0200

    Update test_load_cell_stream.py

--- test_load_cell_stream.py
+++ test_load_cell_stream.py
@@ -1,5 +1,7 @@
-# read_load_cell_directly.py
-# Example usage of load_cell without calling get_forces() or get_moments()
+# motor.py
+# Copyright (c) 2025 Mohsen Khodaee, Oliver Wigger
+
+
 
 import time
 from hardware import load_cell
@@ -14,14 +16,14 @@ load_cell.zero()
 print("\nReading force and moment data directly (without get_forces/get_moments):\n")
 
 try:
-    for _ in range(10):  # Read 10 times
-        fx = load_cell.lc._f[0]
-        fy = load_cell.lc._f[1]
-        fz = load_cell.lc._f[2]
+    for _ in range(100):  # Read 10 times
+        fx = load_cell.lc._f[0]-load_cell.cfg.LC0F[0]
+        fy = load_cell.lc._f[1]-load_cell.cfg.LC0F[1]
+        fz = load_cell.lc._f[2]-load_cell.cfg.LC0F[2]
 
-        mx = load_cell.lc._t[0]
-        my = load_cell.lc._t[1]
-        mz = load_cell.lc._t[2]
+        mx = load_cell.lc._t[0]-load_cell.cfg.LC0T[0]
+        my = load_cell.lc._t[1]-load_cell.cfg.LC0T[1]
+        mz = load_cell.lc._t[2]-load_cell.cfg.LC0T[2]
 
         print(f"Forces (N): Fx={fx:.2f}, Fy={fy:.2f}, Fz={fz:.2f}")
         print(f"Moments (Nm): Mx={mx:.3f}, My={my:.3f}, Mz={mz:.3f}")
